adfdde9962e0a052e2ddaac13ed8e85679659109
MattMakesNoise <matthewjones81@gmail.com>
2023-12-05 16:29:25 +0000

    feat: adds css variables file, includes at the start of the styles gulp task and updates the scss colour variables to use the css variables.

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,11 @@ const uglifyJS    = require('gulp-uglify');
 // Compile Sass
 function styles() {
 	//1. Where is my SCSS file?
-    return src('./src/assets/scss/**/*.scss', {sourcemaps: true})
+    return src(
+			'./src/assets/css/variables.css',
+			'./src/assets/scss/**/*.scss',
+			{sourcemaps: true}
+		)
         //2. Pass the file through compiler
         .pipe(sass().on('error', sass.logError)) // Throw shortened error if you make a mistake
         //3. Where is the compiled CSS output
