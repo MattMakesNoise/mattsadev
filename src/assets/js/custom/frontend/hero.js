5833625c02363f08d0bbb5be0edcb7f90c672606
MattMakesNoise <matthewjones81@gmail.com>
2024-01-08 10:00:57 +0000

    fix: updates hero.js so that code only runs if we're on the homepage.

--- src/assets/js/custom/frontend/hero.js
+++ src/assets/js/custom/frontend/hero.js
@@ -3,49 +3,52 @@ const dots = document.querySelectorAll("[data-carousel-dot]");
 const slides = document.querySelector("[data-slides]");
 let currentIndex = 0;
 
-function changeSlide() {
-	const activeSlide = slides.querySelector("[data-active]");
-	delete activeSlide.dataset.active;
-
-	if (currentIndex >= slides.children.length) currentIndex = 0;
-
-	slides.children[currentIndex].dataset.active = true;
-
-	dots.forEach((dot, index) => {
-		dot.classList.toggle("active", index === currentIndex);
-	});
-
-	currentIndex++;
-}
-
-setInterval(changeSlide, 4000);
-
-
-buttons.forEach(button => {
-	button.addEventListener("click", (e) => {
+// Only run if page is home page
+if (document.body.classList.contains("home")) {
+	function changeSlide() {
 		const activeSlide = slides.querySelector("[data-active]");
-		const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-
-		let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-
-		if (newIndex < 0) newIndex = slides.children.length -1;
-		if (newIndex >= slides.children.length) newIndex = 0;
-
-		slides.children[newIndex].dataset.active = true;
 		delete activeSlide.dataset.active;
-	});
-});
-
-dots.forEach((dot, index) => {
-	dot.addEventListener("click", () => {
-		const newIndex = parseInt(dot.dataset.carouselDot) - 1;
-		const activeSlide = slides.querySelector("[data-active]");
-
-		slides.children[newIndex].dataset.active = true;
-		delete activeSlide.dataset.active;
-
+	
+		if (currentIndex >= slides.children.length) currentIndex = 0;
+	
+		slides.children[currentIndex].dataset.active = true;
+	
 		dots.forEach((dot, index) => {
-			dot.classList.toggle("active", index === newIndex);
+			dot.classList.toggle("active", index === currentIndex);
+		});
+	
+		currentIndex++;
+	}
+	
+	setInterval(changeSlide, 4000);
+	
+	
+	buttons.forEach(button => {
+		button.addEventListener("click", (e) => {
+			const activeSlide = slides.querySelector("[data-active]");
+			const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+	
+			let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+	
+			if (newIndex < 0) newIndex = slides.children.length -1;
+			if (newIndex >= slides.children.length) newIndex = 0;
+	
+			slides.children[newIndex].dataset.active = true;
+			delete activeSlide.dataset.active;
 		});
 	});
-});
+	
+	dots.forEach((dot, index) => {
+		dot.addEventListener("click", () => {
+			const newIndex = parseInt(dot.dataset.carouselDot) - 1;
+			const activeSlide = slides.querySelector("[data-active]");
+	
+			slides.children[newIndex].dataset.active = true;
+			delete activeSlide.dataset.active;
+	
+			dots.forEach((dot, index) => {
+				dot.classList.toggle("active", index === newIndex);
+			});
+		});
+	});
+}
